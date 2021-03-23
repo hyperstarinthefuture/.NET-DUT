9ec3034443b2004b0f6c1eb8d727a047b20cff39
Toàn Đinh <toandinh6501@outlook.com>
2021-03-23 10:17:59 +0700

    10:17 AM -  March 23,2021 Updated

---
+++ Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace ASM_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lb0 = new System.Windows.Forms.Label();
+            this.Lb1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Tbx1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Tbx2 = new System.Windows.Forms.TextBox();
+            this.CBx1 = new System.Windows.Forms.CheckBox();
+            this.Cbb1 = new System.Windows.Forms.ComboBox();
+            this.CBx3 = new System.Windows.Forms.CheckBox();
+            this.CBx2 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lb0
+            // 
+            this.Lb0.AutoSize = true;
+            this.Lb0.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lb0.Location = new System.Drawing.Point(257, 9);
+            this.Lb0.Name = "Lb0";
+            this.Lb0.Size = new System.Drawing.Size(334, 38);
+            this.Lb0.TabIndex = 0;
+            this.Lb0.Text = "Dental Payment Form";
+            this.Lb0.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Lb1
+            // 
+            this.Lb1.AutoSize = true;
+            this.Lb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lb1.Location = new System.Drawing.Point(165, 70);
+            this.Lb1.Name = "Lb1";
+            this.Lb1.Size = new System.Drawing.Size(154, 25);
+            this.Lb1.TabIndex = 0;
+            this.Lb1.Text = "Tên khách hàng";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(413, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Tính tiền";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(555, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Tbx1
+            // 
+            this.Tbx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tbx1.Location = new System.Drawing.Point(362, 70);
+            this.Tbx1.Name = "Tbx1";
+            this.Tbx1.Size = new System.Drawing.Size(257, 30);
+            this.Tbx1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(520, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "$ 100.000";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(504, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "$ 1.200.000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(520, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "$ 200.000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(165, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Số cái";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(165, 315);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tổng tiền";
+            // 
+            // Tbx2
+            // 
+            this.Tbx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Tbx2.Location = new System.Drawing.Point(362, 310);
+            this.Tbx2.Name = "Tbx2";
+            this.Tbx2.ReadOnly = true;
+            this.Tbx2.Size = new System.Drawing.Size(257, 30);
+            this.Tbx2.TabIndex = 0;
+            // 
+            // CBx1
+            // 
+            this.CBx1.AutoSize = true;
+            this.CBx1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBx1.Location = new System.Drawing.Point(170, 124);
+            this.CBx1.Name = "CBx1";
+            this.CBx1.Size = new System.Drawing.Size(101, 29);
+            this.CBx1.TabIndex = 2;
+            this.CBx1.Text = "Cạo vôi";
+            this.CBx1.UseVisualStyleBackColor = true;
+            this.CBx1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Cbb1
+            // 
+            this.Cbb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cbb1.FormattingEnabled = true;
+            this.Cbb1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.Cbb1.Location = new System.Drawing.Point(362, 263);
+            this.Cbb1.Name = "Cbb1";
+            this.Cbb1.Size = new System.Drawing.Size(257, 33);
+            this.Cbb1.TabIndex = 5;
+            // 
+            // CBx3
+            // 
+            this.CBx3.AutoSize = true;
+            this.CBx3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBx3.Location = new System.Drawing.Point(170, 215);
+            this.CBx3.Name = "CBx3";
+            this.CBx3.Size = new System.Drawing.Size(168, 29);
+            this.CBx3.TabIndex = 4;
+            this.CBx3.Text = "Chụp hình răng";
+            this.CBx3.UseVisualStyleBackColor = true;
+            // 
+            // CBx2
+            // 
+            this.CBx2.AutoSize = true;
+            this.CBx2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBx2.Location = new System.Drawing.Point(170, 170);
+            this.CBx2.Name = "CBx2";
+            this.CBx2.Size = new System.Drawing.Size(117, 29);
+            this.CBx2.TabIndex = 3;
+            this.CBx2.Text = "Tẩy trắng";
+            this.CBx2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(264, 381);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Làm mới";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.CBx2);
+            this.Controls.Add(this.CBx3);
+            this.Controls.Add(this.Cbb1);
+            this.Controls.Add(this.CBx1);
+            this.Controls.Add(this.Tbx2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tbx1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Lb1);
+            this.Controls.Add(this.Lb0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lb0;
+        private System.Windows.Forms.Label Lb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Tbx1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Tbx2;
+        private System.Windows.Forms.CheckBox CBx1;
+        private System.Windows.Forms.ComboBox Cbb1;
+        private System.Windows.Forms.CheckBox CBx3;
+        private System.Windows.Forms.CheckBox CBx2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASM_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Cbb1.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?","Thông báo",MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+            {
+                //this.Close();
+                this.Dispose();
+            }    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //double d = 0;
+            //tbx2.text = d.tostring();
+            //Tbx2.Text = Total().ToString();
+            if (Tbx1.Text == "")
+            {
+                MessageBox.Show("Bạn chưa nhập tên khách hàng!");
+            }
+            else
+            {
+                if (CBx1.Checked == false && CBx2.Checked == false && CBx3.Checked == false)
+                {
+                    MessageBox.Show("Bạn chưa chọn dịch vụ chăm sóc răng!");
+                }    
+                else
+                {
+                    if (Convert.ToInt32(Cbb1.SelectedItem.ToString()) == 0)
+                    {
+                        MessageBox.Show("Bạn chưa chọn số răng cần chăm sóc!");
+                    }
+                    else
+                    {
+                        Tbx2.Text = Total().ToString();
+                    }    
+                }    
+            }    
+        }
+        private double Total()
+        {
+            double s = 0;
+            //Code tinh tien
+            if (CBx1.Checked)
+            {
+                s += 100;
+            }
+            if (CBx2.Checked)
+            {
+                s += 1200;
+            }
+            if (CBx3.Checked)
+            {
+                s += 200;
+            }
+            //string select = Cbb1.SelectedItem.ToString();
+            //int index = Cbb1.SelectedIndex;
+            //string select = Cbb1.Items[index].ToString();
+            //int r = Convert.ToInt32(select);
+            //s += r * 80;
+            s += Convert.ToInt32(Cbb1.SelectedItem.ToString()) * 80;
+            return s;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Tbx1.Text = "";
+            Tbx2.Text = "";
+            Cbb1.SelectedIndex = 0;
+            CBx1.Checked = false;
+            CBx2.Checked = false;
+            CBx3.Checked = false;
+        }
+    }
+}
+//Date created: March 22, 2021
+//Last Update: March 23, 2021
